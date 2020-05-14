f001631f473801537f65a89a7460df0719bfff52
Alan Rubin <alan.rubin@wehi.edu.au>
2020-05-14 18:13:38 +1000

    added placeholder example for gene model plot

---
+++ plotsf/main.py
@@ -0,0 +1,10 @@
+import json
+from plotsf.convert.clinvar import read_clinvar_tabular_file
+from plotsf.genemodel.plot import plot_figure
+
+if __name__ == "__main__":
+    # this is a really basic test example
+    with open("example/PTEN.json", "r") as handle:
+        gene = json.load(handle)
+    data = read_clinvar_tabular_file("example/PTEN_clinvar.txt")
+    plot_figure(data, gene, "example/PTEN_test.pdf")
